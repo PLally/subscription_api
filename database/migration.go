e990b48fea3221981846fb4413163f0c1b4aa974
PLlally <11771225+PLally@users.noreply.github.com>
2020-02-19 17:30:47 -0500

    use GORM for dataabse interaction

---
+++ database/migration.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/jinzhu/gorm"
+
+func Migrate(db *gorm.DB) {
+
+	db.AutoMigrate(&SubscriptionType{}, &Destination{}, &Subscription{})
+	db.Model(&Subscription{}).
+	AddForeignKey("destination_id", "destinations(id)", "RESTRICT", "RESTRICT")
+	db.Model(&Subscription{}).
+	AddForeignKey("subscription_type_id", "subscription_types(id)", "RESTRICT", "RESTRICT")
+}
