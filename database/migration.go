28d5a9810f988d4829febf66b55913637caf8890
PLlally <11771225+PLally@users.noreply.github.com>
2020-02-25 00:45:22 -0500

    create gorm models

--- database/migration.go
+++ database/migration.go
@@ -1,12 +1,15 @@
-package main
+package database
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+
+)
 
 func Migrate(db *gorm.DB) {
-
 	db.AutoMigrate(&SubscriptionType{}, &Destination{}, &Subscription{})
+
 	db.Model(&Subscription{}).
-	AddForeignKey("destination_id", "destinations(id)", "RESTRICT", "RESTRICT")
+		AddForeignKey("destination_id", "destinations(id)", "RESTRICT", "RESTRICT")
 	db.Model(&Subscription{}).
-	AddForeignKey("subscription_type_id", "subscription_types(id)", "RESTRICT", "RESTRICT")
+		AddForeignKey("subscription_type_id", "subscription_types(id)", "RESTRICT", "RESTRICT")
 }
