a66c7685d65d7a663a9f05ed97749beeefbf90a8
PLlally <11771225+PLally@users.noreply.github.com>
2020-03-11 22:37:08 -0400

    remove gorm.Model

--- database/models.go
+++ database/models.go
@@ -1,22 +1,30 @@
 package database
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+)
 
 type SubscriptionType struct {
-	gorm.Model
+	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 	Type          string `gorm:"unique_index:idx_type_tags" json:"type"`
 	Tags          string `gorm:"unique_index:idx_type_tags" json:"tags"`
 	Subscriptions []Subscription
 }
 
 type Destination struct {
-	gorm.Model
+	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 	ExternalIdentifier string `gorm:"unique_index:idx_destination_identifier" json:"external_identifier"`
 	DestinationType    string `gorm:"unique_index:idx_destination_identifier" json:"destination_type"`
 }
 
 type Subscription struct {
-	gorm.Model
+	ID        uint `gorm:"primary_key"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 	Destination        Destination
 	SubscriptionType   SubscriptionType
 	DestinationID      uint `gorm:"unique_index:idx_destination_subtype"`
